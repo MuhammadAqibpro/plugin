4d1e0a475b36a47835b1ae724ba80cb70d41dc26
MuhammadAqibpro <muhammadaqibraza183@gmail.com>
2024-10-02 21:04:13 +0500

    Add files via upload
    wp-plugin

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+*plugin Name: admin remover
+*plugin URL :https://wordpress.org/plugins/
+*Author :aqibraza
+*Description:  https://aqibrza.wordpress.com/
+*Version: 1.0
+*Text Domain : hello-world
+*/
+{
+show-admin-bar(false);
+}
+?>
